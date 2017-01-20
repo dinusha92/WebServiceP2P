841960f8d3ddce53cb5018d75225c338564ee696
dinusha92 <dinudiss@gmail.com>
2017-01-21 02:35:24 +0530

    Adding Neighbour

---
+++ src/main/java/Neighbour.java
@@ -0,0 +1,35 @@
+class Neighbour{
+    private String ip;
+    private int port;
+    private String username;
+
+    public Neighbour(String ip, int port, String username){
+        this.ip = ip;
+        this.port = port;
+        this.username = username;
+    }
+
+    public String getIp(){
+        return this.ip;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public int getPort(){
+        return this.port;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null || getClass() != obj.getClass())
+            return false;
+        Neighbour neighbour = (Neighbour) obj;
+        if(port!=neighbour.getPort())
+            return false;
+        else if(ip.equals(neighbour.getIp()))
+            return true;
+        else return false;
+    }
+}
