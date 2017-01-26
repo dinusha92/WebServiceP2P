65848c3c250b05c1a1302082d538815777d04ac4
kasubdn <kasun.bdn@gmail.com>
2017-01-26 17:23:14 +0530

    refactoring

--- src/main/java/com/uom/cse/distsearch/Node.java
+++ src/main/java/com/uom/cse/distsearch/Node.java
@@ -7,7 +7,9 @@ public class Node implements Serializable {
     private int port;
     private String username;
     
-
+    public Node() {
+		// TODO Auto-generated constructor stub
+	}
 
     public Node(String ip, int port) {
         this.ip = ip;
@@ -24,16 +26,25 @@ public class Node implements Serializable {
         return port;
     }
 
+    public void setPort(int port) {
+        this.port = port;
+    }
 
     public String getIp() {
         return ip;
     }
 
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String url() {
         return String.format("http://%s:%d/", ip, port);
